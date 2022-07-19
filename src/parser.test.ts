f2c5719b9fc79752edbc69ae0cc838e2ae113cbf
SeR <44164289+OraUser@users.noreply.github.com>
2022-07-19 17:31:51 +0300

    Update parser.test.ts
    Починил тесты, добавил false часть

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -1,14 +1,13 @@
-const { parser } = jest.requireActual('./parser');
-
-import {   opSum,
-           opMinus,
-           opMultiply,
-           opDevide
-} from "./parser";
-
+const { parser,
+        opSum,
+        opMinus,
+        opMultiply,
+        opDevide
+} = jest.requireActual("./parser");
 
 
-describe("Проверка разбора строки и операторов", () => {
+
+describe("Проверка разбора строки и операторов (true)", () => {
   it("1 + 11", () => {
     expect(parser('1 + 11')).toEqual([1, '+', 11]);
   });
@@ -22,24 +21,58 @@ describe("Проверка разбора строки и операторов",
   });
 });
 
+describe("Проверка разбора строки и операторов (false)", () => {
+  it("1 + 1", () => {
+    expect(parser('1 + 1')).toEqual(11);
+  });
+
+  it("1 - 1", () => {
+    expect(parser("1 - 1")).toEqual(1);
+  });
+
+  it("1 + 1 - 1 + 11", () => {
+    expect(parser("1 + 1 - 1 + 11")).toEqual(11);
+  });
+});
 
 
-describe("Тесты работы операторов", () => {
+
+describe("Тесты работы операторов (true)", () => {
 
   it("2 + 1 = 3", () => {
-    expect(opSum(2, 1)).toBe(3);
+    expect(opSum(2, 1)).toEqual(3);
   });
 
   it("2 - 1 = 1", () => {
-    expect(opMinus(2, 1)).toBe(1);
+    expect(opMinus(2, 1)).toEqual(1);
   });
 
   it("2 * 2 = 4", () => {
-    expect(opMultiply(2, 2)).toBe(4);
+    expect(opMultiply(2, 2)).toEqual(4);
   });
 
   it("6 / 3 = 2", () => {
-    expect(opMultiply(6, 3)).toBe(2);
+    expect(opDevide(6, 3)).toEqual(2);
+  });
+
+});
+
+describe("Тесты работы операторов (false)", () => {
+
+  it("1 + 1 = 11", () => {
+    expect(opSum(1, 1)).toEqual(11);
+  });
+
+  it("1 - 1 = 1", () => {
+    expect(opMinus(1, 1)).toEqual(1);
+  });
+
+  it("2 * 2 = 1", () => {
+    expect(opMultiply(2, 2)).toEqual(1);
+  });
+
+  it("6 / 3 = 18", () => {
+    expect(opDevide(6, 3)).toEqual(18);
   });
 
 });
